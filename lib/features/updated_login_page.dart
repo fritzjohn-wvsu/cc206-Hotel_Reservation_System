427df5e84e2e871719d58619093da1cfffa1db44
dales1904 <dave.ciales@wvsu.edu.ph>
2024-10-28 21:33:37 +0800

    INTERACTION ADDED

--- lib/features/updated_login_page.dart
+++ lib/features/updated_login_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+final _formKey = GlobalKey<FormState>();
+
 class updateLogin extends StatelessWidget {
   const updateLogin({super.key});
 
@@ -13,8 +15,27 @@ class updateLogin extends StatelessWidget {
   }
 }
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({super.key});
+
+  @override
+  _LoginState createState() => _LoginState();
+  
+}
+
+class _LoginState extends State<Login> {
+
+  bool _isSecurePassword = true;
+  //validation for email
+  String? validateEmail(String? email) {
+    RegExp emailRegex = RegExp(r'^[\w\.-]+@[\w-]+\.\w{2,3}(\.\w{2,3})?$');
+    final isEmailValid = emailRegex.hasMatch(email ?? '');
+    if (!isEmailValid) {
+      return 'Please enter a valid email';
+    }
+    return null;
+  
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +43,9 @@ class Login extends StatelessWidget {
       backgroundColor: Colors.white,
       body: Padding(
         padding: const EdgeInsets.all(16.0),
+
+        child: Form(
+          key: _formKey,
         child: Column(
           children: [
             const SizedBox(height: 80),
@@ -40,9 +64,10 @@ class Login extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 50),
+            //EmailTextfield
             SizedBox(
               width: 500,
-              child: TextField(
+              child: TextFormField(
                 keyboardType: TextInputType.emailAddress,
                 decoration: InputDecoration(
                   labelText: 'Email',
@@ -50,26 +75,32 @@ class Login extends StatelessWidget {
                   prefixIcon: Icon(Icons.mail),
                   border: OutlineInputBorder(),
                 ),
+                validator: validateEmail,
               ),
             ),
             const SizedBox(height: 20),
             SizedBox(
               width: 500,
-              child: TextField(
-                obscureText: true,
+              child: TextFormField(
+                obscureText: _isSecurePassword,
                 decoration: InputDecoration(
                   labelText: 'Password',
                   hintText: 'Enter your password',
                   prefixIcon: Icon(Icons.lock),
                   border: OutlineInputBorder(),
+                   suffixIcon: togglePassword(),
                 ),
+                validator: (pass) => pass!.length <= 8 ? 'Password should contain more than 8 characters' : null,
+               
               ),
             ),
             const SizedBox(height: 35),
             SizedBox(
               width: 200,
               child: ElevatedButton(
-                onPressed: () {},
+                onPressed: () {
+                  _formKey.currentState!.validate();
+                },
                 style: ElevatedButton.styleFrom(
                   padding: const EdgeInsets.symmetric(vertical: 25),
                   shape: RoundedRectangleBorder(
@@ -112,7 +143,20 @@ class Login extends StatelessWidget {
             ),
           ],
         ),
+        ),
       ),
     );
   }
+
+  Widget togglePassword() {
+    return IconButton(onPressed: () {
+      setState(() {
+         _isSecurePassword = !_isSecurePassword;
+      });
+
+    }, icon: _isSecurePassword ? Icon(Icons.visibility) : Icon(Icons.visibility_off));
+  }
 }
+
+  
+
