b8defae893e4d239ef5d57aeaf16d0f6cccb3c22
fritzjohn-wvsu <fritzjohn.simpas@wvsu.edu.ph>
2024-10-28 19:46:18 +0800

    Updated new file

--- lib/features/update_sign_up_page.dart
+++ lib/features/update_sign_up_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+//make the password store
+final _formKey = GlobalKey<FormState>();
+
 void main() {
   runApp(SignUp());
 }
@@ -14,8 +17,32 @@ class SignUp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //store the password
+  final TextEditingController _passwordController = TextEditingController();
+  //store the confirm password
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+
+  bool _isPasswordVisible = false;
+  bool _isConfirmPasswordVisible = false;
+
+  //validate the email
+  String? validateEmail(String? email) {
+    RegExp emailRegex = RegExp(r'^[\w\.-]+@[\w-]+\.\w{2,3}(\.\w{2,3})?$');
+    final isEmailValid = emailRegex.hasMatch(email ?? '');
+    if (!isEmailValid) {
+      return 'Please enter a valid email';
+    }
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,187 +50,220 @@ class HomePage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              // About Me section
-              Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(
-                        top: 10, left: 50, right: 50, bottom: 50),
-                  ),
-                  Column(
-                    children: [
-                      Text(
-                        "SIGN IN",
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 50,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        "Create Your Account!",
-                        style: TextStyle(color: Colors.black87, fontSize: 15),
-                      )
-                    ],
-                  )
-                ],
-              ),
-              // Username field
-              SizedBox(height: 50),
-              Container(
-                width: 500, // Set desired width here
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                    prefixIcon: Padding(
-                      padding: EdgeInsets.all(0.0),
-                      child: Icon(
-                        Icons.person,
-                        color: Colors.black,
-                      ),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(
+                          top: 10, left: 50, right: 50, bottom: 50),
                     ),
-                    border: OutlineInputBorder(),
-                    labelText: 'Username',
+                    Column(
+                      children: [
+                        Text(
+                          "SIGN IN",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 50,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "Create Your Account!",
+                          style: TextStyle(color: Colors.black87, fontSize: 15),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                SizedBox(height: 50),
+                Container(
+                  width: 500,
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      prefixIcon: Padding(
+                        padding: EdgeInsets.all(0.0),
+                        child: Icon(
+                          Icons.person,
+                          color: Colors.black,
+                        ),
+                      ),
+                      border: OutlineInputBorder(),
+                      labelText: 'Username',
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 20),
-              // Email field
-              Container(
-                width: 500, // Set desired width here
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                    prefixIcon: Padding(
-                      padding: EdgeInsets.all(0.0),
-                      child: Icon(
-                        Icons.email,
-                        color: Colors.black,
+                SizedBox(height: 20),
+                Container(
+                  width: 500,
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      prefixIcon: Padding(
+                        padding: EdgeInsets.all(0.0),
+                        child: Icon(
+                          Icons.email,
+                          color: Colors.black,
+                        ),
                       ),
+                      border: OutlineInputBorder(),
+                      labelText: 'E-mail Address',
                     ),
-                    border: OutlineInputBorder(),
-                    labelText: 'E-mail Address',
+                    validator: validateEmail,
                   ),
                 ),
-              ),
-              // Password field
-              SizedBox(height: 20),
-              Container(
-                width: 500, // Set desired width here
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                    prefixIcon: Padding(
-                      padding: EdgeInsets.all(0.0),
-                      child: Icon(
-                        Icons.lock,
-                        color: Colors.black,
+                SizedBox(height: 20),
+                Container(
+                  width: 500,
+                  child: TextFormField(
+                    controller: _passwordController,
+                    obscureText: !_isPasswordVisible,
+                    decoration: InputDecoration(
+                      prefixIcon: const Padding(
+                        padding: EdgeInsets.all(0.0),
+                        child: Icon(
+                          Icons.lock,
+                          color: Colors.black,
+                        ),
                       ),
-                    ),
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                  ),
-                  obscureText: true, // Added for password concealment
-                ),
-              ),
-              // Confirm Password field
-              SizedBox(height: 20),
-              Container(
-                width: 500, // Set desired width here
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                    prefixIcon: Padding(
-                      padding: EdgeInsets.all(0.0),
-                      child: Icon(
-                        Icons.check,
-                        color: Colors.black,
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _isPasswordVisible
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                          color: Colors.black,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _isPasswordVisible = !_isPasswordVisible;
+                          });
+                        },
                       ),
+                      border: const OutlineInputBorder(),
+                      labelText: 'Password',
                     ),
-                    border: OutlineInputBorder(),
-                    labelText: 'Confirm Password',
+                    validator: (pass) => (pass == null || pass.length < 8)
+                        ? 'Password should contain more than 8 characters'
+                        : null,
                   ),
-                  obscureText: true, // Added for password concealment
                 ),
-              ),
-
-              SizedBox(height: 20),
-              //Sign in button
-              Container(
-                width: 175,
-                height: 70,
-                padding:
-                    EdgeInsets.only(top: 14, left: 10, right: 10, bottom: 10),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor:
-                        Colors.black, // Change this to your desired color
+                SizedBox(height: 20),
+                Container(
+                  width: 500,
+                  child: TextFormField(
+                    controller: _confirmPasswordController,
+                    obscureText: !_isConfirmPasswordVisible,
+                    decoration: InputDecoration(
+                      prefixIcon: const Padding(
+                        padding: EdgeInsets.all(0.0),
+                        child: Icon(
+                          Icons.check,
+                          color: Colors.black,
+                        ),
+                      ),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _isConfirmPasswordVisible
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                          color: Colors.black,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _isConfirmPasswordVisible =
+                                !_isConfirmPasswordVisible;
+                          });
+                        },
+                      ),
+                      border: const OutlineInputBorder(),
+                      labelText: 'Confirm Password',
+                    ),
+                    validator: (conpass) {
+                      if (conpass != _passwordController.text) {
+                        return 'Passwords do not match';
+                      }
+                      return null;
+                    },
                   ),
-                  onPressed: () {},
+                ),
+                SizedBox(height: 20),
+                Container(
+                  width: 175,
+                  height: 70,
+                  padding:
+                      EdgeInsets.only(top: 14, left: 10, right: 10, bottom: 10),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                    ),
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        // Proceed with sign up
+                      }
+                    },
+                    child: Text(
+                      'Sign In',
+                      style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
                   child: Text(
-                    'Sign In',
+                    "or",
                     style: TextStyle(
-                        fontSize: 17,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white),
-                  ),
-                ),
-              ),
-
-              Container(
-                padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-                child: Text(
-                  "or",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-
-              //Sign in button
-              Container(
-                width: 200,
-                height: 60,
-                padding: EdgeInsets.only(top: 5, left: 5, right: 5, bottom: 10),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor:
-                        Colors.black, // Change this to your desired color
-                  ),
-                  onPressed: () {},
-                  child: Text(
-                    'Sign in with Google',
-                    style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Already have an Account?",
-                    style: TextStyle(
-                      fontSize: 15,
                       color: Colors.black,
+                      fontSize: 20,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.white,
+                ),
+                Container(
+                  width: 200,
+                  height: 60,
+                  padding:
+                      EdgeInsets.only(top: 5, left: 5, right: 5, bottom: 10),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
                     ),
                     onPressed: () {},
-                    child: Text('Log in'),
-                  )
-                ],
-              ),
-            ],
+                    child: Text(
+                      'Sign in with Google',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Already have an Account?",
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.white,
+                      ),
+                      onPressed: () {},
+                      child: Text('Log in'),
+                    )
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
---
+++ sign_up_simpas_villanueva/lib/features/sign_up_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SignUpPage());
+}
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(children: [
+      Text("Sign Up"),
+      TextField(decoration: InputDecoration(labelText: "Username")),
+      TextField(decoration: InputDecoration(labelText: "Password")),
+      TextButton(
+          onPressed: () {
+//Act when the button is pressed
+          },
+          child: const Text(
+            "Click Me!",
+          )),
+
+      // Put the list of widgets here, separated by commas
+    ]));
+  }
+}
