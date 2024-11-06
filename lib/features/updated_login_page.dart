b8a7a7c4ba418326ab2f81d14be13f7b3f1a4968
Lizy-Joy <lizyjoy.ponce@wvsu.edu.ph>
2024-11-06 12:44:11 +0800

    added button: sign in and forgot password

--- lib/features/updated_login_page.dart
+++ lib/features/updated_login_page.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 final _formKey = GlobalKey<FormState>();
 
-class updateLogin extends StatelessWidget {
-  const updateLogin({super.key});
+class UpdateLogin extends StatelessWidget {
+  const UpdateLogin({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -20,13 +20,12 @@ class Login extends StatefulWidget {
 
   @override
   _LoginState createState() => _LoginState();
-  
 }
 
 class _LoginState extends State<Login> {
-
   bool _isSecurePassword = true;
-  //validation for email
+
+  // Validation for email
   String? validateEmail(String? email) {
     RegExp emailRegex = RegExp(r'^[\w\.-]+@[\w-]+\.\w{2,3}(\.\w{2,3})?$');
     final isEmailValid = emailRegex.hasMatch(email ?? '');
@@ -34,7 +33,6 @@ class _LoginState extends State<Login> {
       return 'Please enter a valid email';
     }
     return null;
-  
   }
 
   @override
@@ -43,120 +41,148 @@ class _LoginState extends State<Login> {
       backgroundColor: Colors.white,
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-
         child: Form(
           key: _formKey,
-        child: Column(
-          children: [
-            const SizedBox(height: 80),
-            const Center(
-              child: Text(
-                'Welcome',
-                style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            const Center(
-              child: Text(
-                'Enter your credential to login',
-                style: TextStyle(fontSize: 15),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            const SizedBox(height: 50),
-            //EmailTextfield
-            SizedBox(
-              width: 500,
-              child: TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  hintText: 'Enter your email',
-                  prefixIcon: Icon(Icons.mail),
-                  border: OutlineInputBorder(),
+          child: Column(
+            children: [
+              const SizedBox(height: 80),
+              const Center(
+                child: Text(
+                  'Welcome',
+                  style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
                 ),
-                validator: validateEmail,
               ),
-            ),
-            const SizedBox(height: 20),
-            SizedBox(
-              width: 500,
-              child: TextFormField(
-                obscureText: _isSecurePassword,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  hintText: 'Enter your password',
-                  prefixIcon: Icon(Icons.lock),
-                  border: OutlineInputBorder(),
-                   suffixIcon: togglePassword(),
+              const Center(
+                child: Text(
+                  'Enter your credential to login',
+                  style: TextStyle(fontSize: 15),
+                  textAlign: TextAlign.center,
                 ),
-                validator: (pass) => pass!.length <= 8 ? 'Password should contain more than 8 characters' : null,
-               
               ),
-            ),
-            const SizedBox(height: 35),
-            SizedBox(
-              width: 200,
-              child: ElevatedButton(
-                onPressed: () {
-                  _formKey.currentState!.validate();
-                },
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.symmetric(vertical: 25),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50),
+              const SizedBox(height: 50),
+              // Email TextField
+              SizedBox(
+                width: 500,
+                child: TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    hintText: 'Enter your email',
+                    prefixIcon: Icon(Icons.mail),
+                    border: OutlineInputBorder(),
                   ),
-                  backgroundColor: Colors.black,
+                  validator: validateEmail,
                 ),
-                child: const Text(
-                  "Log In",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 17,
-                    fontWeight: FontWeight.bold,
+              ),
+              const SizedBox(height: 20),
+              // Password TextField
+              SizedBox(
+                width: 500,
+                child: TextFormField(
+                  obscureText: _isSecurePassword,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    hintText: 'Enter your password',
+                    prefixIcon: Icon(Icons.lock),
+                    border: OutlineInputBorder(),
+                    suffixIcon: togglePassword(),
+                  ),
+                  validator: (pass) =>
+                      pass!.length <= 8 ? 'Password should contain more than 8 characters' : null,
+                ),
+              ),
+              const SizedBox(height: 35),
+              // Login Button
+              SizedBox(
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: () {
+                    _formKey.currentState!.validate();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(vertical: 25),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    backgroundColor: Colors.black,
+                  ),
+                  child: const Text(
+                    "Log In",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 100),
-            const Center(
-              child: Text(
-                'Forgot password?',
-                style: TextStyle(
-                  fontSize: 12,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.grey,
+              const SizedBox(height: 100),
+              // Forgot Password Button
+              Center(
+                child: TextButton(
+                  onPressed: () {
+                    // Define what happens when the 'Forgot password?' button is pressed
+                  },
+                  child: const Text(
+                    'Forgot password?',
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
                 ),
-                textAlign: TextAlign.center,
               ),
-            ),
-            const SizedBox(height: 20),
-            const Center(
-              child: Text(
-                'Don\'t have an account?  Sign In',
-                style: TextStyle(
-                  fontSize: 12,
-                  fontWeight: FontWeight.bold,
+              const SizedBox(height: 20),
+              // Sign In Section
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Don\'t have an account?',
+                      style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        // Define what happens when the 'Sign In' button is pressed
+                      },
+                      child: const Text(
+                        'Sign In',
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue, // Optional color to make it stand out as a button
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ],
                 ),
-                textAlign: TextAlign.center,
               ),
-            ),
-          ],
-        ),
+            ],
+          ),
         ),
       ),
     );
   }
 
+  // Toggle Password Visibility
   Widget togglePassword() {
-    return IconButton(onPressed: () {
-      setState(() {
-         _isSecurePassword = !_isSecurePassword;
-      });
-
-    }, icon: _isSecurePassword ? Icon(Icons.visibility) : Icon(Icons.visibility_off));
+    return IconButton(
+      onPressed: () {
+        setState(() {
+          _isSecurePassword = !_isSecurePassword;
+        });
+      },
+      icon: _isSecurePassword ? Icon(Icons.visibility) : Icon(Icons.visibility_off),
+    );
   }
 }
-
-  
-
